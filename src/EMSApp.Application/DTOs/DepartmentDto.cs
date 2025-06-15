11d3befa490d99f8281a2635d1a7e3cafadf6051
Radu Muntean <radumuntean03@gmail.com>
2025-06-15 13:17:47 +0300

    fix

--- src/EMSApp.Application/DTOs/DepartmentDto.cs
+++ src/EMSApp.Application/DTOs/DepartmentDto.cs
@@ -1,10 +1,6 @@
 ﻿namespace EMSApp.Application;
 
-<<<<<<< HEAD
-public class DepartmentDto(
-=======
 public sealed record DepartmentDto(
->>>>>>> 73f68f78fad61200a38ac8e657024323c185547d
     string Id,
     string Name,
     string ManagerId,
